57c5cbdf904a94b447d25dc084948daf16cb24bf
Andres-mmg <andres.martinez.g@gmail.com>
2024-12-18 12:42:49 -0300

    Squashed 'OkFugas.AdminPlus/' content from commit 7757ee3
    git-subtree-dir: OkFugas.AdminPlus
    git-subtree-split: 7757ee3b2979f251f1274cce78ae0b2e50a860ea

---
+++ pbcodegen/sdk/pocketbase-csharp-sdk/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace pocketbase_csharp_sdk.Models
+{
+    public class UserModel : BaseAuthModel
+    {
+        [JsonPropertyName("avatar")]
+        public string? Avatar { get; set; }
+        
+    }
+}
